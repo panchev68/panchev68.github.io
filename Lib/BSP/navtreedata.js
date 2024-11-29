d202170b848a12f01ed4622918ef4efc35317ca6
Боян Панчев <panchev68@gmail.com>
2024-11-30 00:56:49 +0200

    update

--- Lib/BSP/navtreedata.js
+++ Lib/BSP/navtreedata.js
@@ -24,19 +24,16 @@
 */
 var NAVTREE =
 [
-  [ "BSP Library", "index.html", [
+  [ "Lib::BSP", "index.html", [
     [ "Board Supported Peripherals Library", "index.html", null ],
     [ "Implement Digital Peripheral Driver", "d8/ddd/implement-digital-peripheral-driver.html", null ],
     [ "Deprecated List", "da/d58/deprecated.html", null ],
     [ "Topics", "topics.html", "topics" ],
-    [ "Namespaces", "namespaces.html", [
-      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
-      [ "Namespace Members", "namespacemembers.html", [
-        [ "All", "namespacemembers.html", null ],
-        [ "Typedefs", "namespacemembers_type.html", null ],
-        [ "Enumerations", "namespacemembers_enum.html", null ],
-        [ "Enumerator", "namespacemembers_eval.html", null ]
-      ] ]
+    [ "Namespace Members", "namespacemembers.html", [
+      [ "All", "namespacemembers.html", null ],
+      [ "Typedefs", "namespacemembers_type.html", null ],
+      [ "Enumerations", "namespacemembers_enum.html", null ],
+      [ "Enumerator", "namespacemembers_eval.html", null ]
     ] ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
@@ -59,11 +56,8 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "annotated.html",
-"d1/d59/namespace_lib_1_1_b_s_p_1_1_display.html#afe2d246e2a2a5bc3699440ae4548515ba3f35d59b6b7629dfe280f0d4788c912b",
-"d2/d31/struct_lib_1_1_b_s_p_1_1_x_p_t2046_1_1_version1_1_1_driver_1_1_parameters.html#a3ed4c24247b93e9a2c9827909ae4fb83",
-"d6/d88/union_lib_1_1_b_s_p_1_1_ethercat_1_1_p_d_i_1_1_l_a_n9252_1_1_helper_1_1_u_u32.html#a8ca465c9a265f74b3764b0a1f9ad8822",
-"da/d45/class_lib_1_1_b_s_p_1_1_ethercat_1_1_p_d_i_1_1_l_a_n9252_1_1_base.html#ad040cf1ec4eff98a2e4d598b65a0fd7f",
-"df/d2d/union_lib_1_1_b_s_p_1_1_ethercat_1_1_p_d_i_1_1_l_a_n9252_1_1_helper_1_1_u_u16.html"
+"d6/d88/union_lib_1_1_b_s_p_1_1_ethercat_1_1_p_d_i_1_1_l_a_n9252_1_1_helper_1_1_u_u32.html#a654e5d7ab0613ef1fb28769a1ed73de2",
+"dc/de7/class_lib_1_1_b_s_p_1_1_termometer_1_1_d_t_h_1_1_driver_3_01_type_1_1_d_t_h22_01_4.html#a8ae02b3f89e4456053b20adcea8d4e4b"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
