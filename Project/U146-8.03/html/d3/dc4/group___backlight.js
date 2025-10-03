f580f65417e9fb5ebb4f3bef4a939c21de8d31cc
Боян Панчев <panchev68@gmail.com>
2025-10-03 13:02:50 +0300

    Update U146

---
+++ Project/U146-8.03/html/d3/dc4/group___backlight.js
@@ -0,0 +1,41 @@
+var group___backlight =
+[
+    [ "Lib::BSP::Backlight", "d4/d1f/class_lib_1_1_b_s_p_1_1_backlight.html", [
+      [ "Backlight", "d3/dc4/group___backlight.html#gae1576365076fd9cb0802723cef651d0a", null ],
+      [ "~Backlight", "d3/dc4/group___backlight.html#gacb6486a968fee440125df9e98fe9612c", null ],
+      [ "disableDimmer", "d3/dc4/group___backlight.html#ga81d580b32630e8dab646f7ba2cfa8de5", null ],
+      [ "enableDimmer", "d3/dc4/group___backlight.html#ga87973d67ac321509088552d8c227f9b6", null ],
+      [ "get", "d3/dc4/group___backlight.html#ga989cb4fb6805cb41d7ddd6229290fb1d", null ],
+      [ "init", "d3/dc4/group___backlight.html#ga7887d56a6dc55d47feee973e4801791c", null ],
+      [ "operator=", "d3/dc4/group___backlight.html#ga5bb178db31ff9430043d7d7e7d5a3ddd", null ],
+      [ "restartDimmer", "d3/dc4/group___backlight.html#ga491713aac10b5443307a08542e206418", null ],
+      [ "set", "d3/dc4/group___backlight.html#gaca96c1b90042a7bbc5f858c53871c870", null ],
+      [ "setupDimmer", "d3/dc4/group___backlight.html#ga25ae8c8c88fb0a05745926c0dc299ab5", null ],
+      [ "tick", "d3/dc4/group___backlight.html#ga448528b7400740d41c95cc74ecef1a87", null ]
+    ] ],
+    [ "Lib::BSP::Backlight::Config", "db/d19/struct_lib_1_1_b_s_p_1_1_backlight_1_1_config.html", [
+      [ "dimmerMax", "d3/dc4/group___backlight.html#ga35479166e7a914b7471de66b17cc8d68", null ],
+      [ "dimmerMin", "d3/dc4/group___backlight.html#ga16c87d9d2094c1fa109880e195a13b51", null ],
+      [ "fadeStep", "d3/dc4/group___backlight.html#ga4b995f8dc4809e1442e1600da5a7fb7b", null ],
+      [ "sleepTime", "d3/dc4/group___backlight.html#gad30dd39f20169c3d91c056daced3a362", null ],
+      [ "timerChannel", "d3/dc4/group___backlight.html#gac0d932741746d6e82962fc930d8c1f16", null ],
+      [ "timerHandle", "d3/dc4/group___backlight.html#ga1b8b4c4a18b711fd58c916b24535f7c0", null ]
+    ] ],
+    [ "Lib::BSP::Backlight::Backlight", "d3/dc4/group___backlight.html#gae1576365076fd9cb0802723cef651d0a", null ],
+    [ "Lib::BSP::Backlight::~Backlight", "d3/dc4/group___backlight.html#gacb6486a968fee440125df9e98fe9612c", null ],
+    [ "Lib::BSP::Backlight::disableDimmer", "d3/dc4/group___backlight.html#ga81d580b32630e8dab646f7ba2cfa8de5", null ],
+    [ "Lib::BSP::Backlight::enableDimmer", "d3/dc4/group___backlight.html#ga87973d67ac321509088552d8c227f9b6", null ],
+    [ "Lib::BSP::Backlight::get", "d3/dc4/group___backlight.html#ga989cb4fb6805cb41d7ddd6229290fb1d", null ],
+    [ "Lib::BSP::Backlight::init", "d3/dc4/group___backlight.html#ga7887d56a6dc55d47feee973e4801791c", null ],
+    [ "Lib::BSP::Backlight::operator=", "d3/dc4/group___backlight.html#ga5bb178db31ff9430043d7d7e7d5a3ddd", null ],
+    [ "Lib::BSP::Backlight::restartDimmer", "d3/dc4/group___backlight.html#ga491713aac10b5443307a08542e206418", null ],
+    [ "Lib::BSP::Backlight::set", "d3/dc4/group___backlight.html#gaca96c1b90042a7bbc5f858c53871c870", null ],
+    [ "Lib::BSP::Backlight::setupDimmer", "d3/dc4/group___backlight.html#ga25ae8c8c88fb0a05745926c0dc299ab5", null ],
+    [ "Lib::BSP::Backlight::tick", "d3/dc4/group___backlight.html#ga448528b7400740d41c95cc74ecef1a87", null ],
+    [ "Lib::BSP::Backlight::Config::dimmerMax", "d3/dc4/group___backlight.html#ga35479166e7a914b7471de66b17cc8d68", null ],
+    [ "Lib::BSP::Backlight::Config::dimmerMin", "d3/dc4/group___backlight.html#ga16c87d9d2094c1fa109880e195a13b51", null ],
+    [ "Lib::BSP::Backlight::Config::fadeStep", "d3/dc4/group___backlight.html#ga4b995f8dc4809e1442e1600da5a7fb7b", null ],
+    [ "Lib::BSP::Backlight::Config::sleepTime", "d3/dc4/group___backlight.html#gad30dd39f20169c3d91c056daced3a362", null ],
+    [ "Lib::BSP::Backlight::Config::timerChannel", "d3/dc4/group___backlight.html#gac0d932741746d6e82962fc930d8c1f16", null ],
+    [ "Lib::BSP::Backlight::Config::timerHandle", "d3/dc4/group___backlight.html#ga1b8b4c4a18b711fd58c916b24535f7c0", null ]
+];
